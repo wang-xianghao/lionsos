2349f1399bd2b4d57c73e49c57ebae87cacd12ab
xianghao <wang.xianghao@outlook.com>
2024-09-12 15:59:08 +1000

    connected client with serial sddf driver

--- components/microkitlibc/backend.c
+++ components/microkitlibc/backend.c
@@ -1,9 +1,28 @@
 #include <microkit.h>
 
+#include <sddf/serial/queue.h>
+#include <serial_config.h>
+#include <sddf/util/printf.h>
+
+/*
+ * Shared regions for Serial communication
+ */
+char *serial_rx_data;
+char *serial_tx_data;
+serial_queue_t *serial_rx_queue;
+serial_queue_t *serial_tx_queue;
+serial_queue_handle_t serial_rx_queue_handle;
+serial_queue_handle_t serial_tx_queue_handle;
+
 void init(void) {
     microkit_dbg_puts("Microkit C Library initializes...");
+
+    serial_cli_queue_init_sys(microkit_name, &serial_rx_queue_handle,
+                            serial_rx_queue, serial_rx_data,
+                            &serial_tx_queue_handle, serial_tx_queue,
+                            serial_tx_data);
 }
 
 void notified(microkit_channel ch) {
-
+    
 }
